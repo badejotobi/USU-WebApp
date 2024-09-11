f84ab314bf5d455881f294ea027d9746fa6618a1
badejotobi <133620914+badejotobi@users.noreply.github.com>
2024-09-11 13:43:24 -0700

    Update app.py

--- Capstone Project work/app.py
+++ Capstone Project work/app.py
@@ -32,4 +32,4 @@ def equipment():
     return render_template('equipment.html', equipment_data=equipment_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port="8080",debug=True)
